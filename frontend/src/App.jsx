c7a8114eb3a22ab8bfdf4429693719e6d565818c
SanjanaNeware08 <newaress08@gmail.com>
2025-04-28 01:30:46 +0530

    Update App.jsx

---
+++ frontend/src/App.jsx
@@ -0,0 +1,21 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Login from './pages/Login';
+import Signup from './pages/Signup';
+import Dashboard from './pages/Dashboard';
+import ProjectPage from './pages/ProjectPage';
+import ProtectedRoute from './components/ProtectedRoute';
+
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/" element={<ProtectedRoute><Dashboard /></ProtectedRoute>} />
+        <Route path="/project/:projectId" element={<ProtectedRoute><ProjectPage /></ProtectedRoute>} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
